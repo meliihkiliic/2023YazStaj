275309b18fc9f5ef6ee962f44b6b2a6eea4a63c7
Melih <115799467+meliihkiliic@users.noreply.github.com>
2023-07-24 11:36:38 +0300

    Ornek Servlet ve Listener
    Açıldığında ve kapandığında log tutar.

---
+++ Servlet Listener/src/main/java/com/servlet/OrnekServlet.java
@@ -0,0 +1,28 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OrnekServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Örnek Servlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>ornek</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
